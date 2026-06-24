e0112a29471bb9944f109b6b14a84e4e83b0964a
SuzannaDias <Suzanna.dias77@gmail.com>
2026-06-24 18:33:57 -0300

    Refactor verificar function and add carregar function

--- script.js
+++ script.js
@@ -1,49 +1,22 @@
-function verificar(){
-    var data= new Date()
-    var ano = data.getFullYear()
-    var fano= document.getElementById('txtano')
-    var res= document.getElementById('res')
-    if(fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('{ERRO} Verifique os dados e tente novamente')
-    } else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        var img= document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if(fsex[0].checked){
-            gênero = 'Homem'
-             if (idade >= 0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'bebeF.png')
-            } else if(idade < 21){
-                //jovem
-                img.setAttribute('src', 'jovemM.png')
-            } else if ( idade < 50){
-                //Adulto
-                img.setAttribute('src', 'idosoM.png')
-            }else{
-                img.setAttribute('src', 'idosoM.png')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 13
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background =' #e2cd9f'
+        
+    
+} else if (hora >= 12 && hora <= 18) {
+        img.src = 'fototarde.png'
+                document.body.style.background = '#e37c06 '
 
-            }
-        } else if (fsex[1].checked) {
-            gênero = 'Mulher'
-            if (idade >= 0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'bebeF.png')
-            } else if(idade < 21){
-                //jovem
-                img.setAttribute('src', 'jovemF.png')
-            } else if ( idade < 50){
-                //Adulto
-                img.setAttribute('src', 'idosoF.png')
-            }else{
-                img.setAttribute('src', 'idosoF.png')
-            }
-        }
-        res.style.textAlign ='center'
-        res.innerHTML = `Você é um(a) ${gênero} com ${idade} anos`
-        res.appendChild(img)
+    } else {
+        img.src = 'fotonoite.png'
+                document.body.style.background = '#1c1832 '
+
     }
- 
 }
